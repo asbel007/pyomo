0c1306ff3b405d897f1bb8a32b659238b63a43e7
asbel007 <dreik.elm.mec.game2@gmail.com>
2018-04-09 12:10:40 -0400

    modifi

--- energia.py
+++ energia.py
@@ -8,3 +8,16 @@ model.price=Set()
 
 model.Cap=Param()
 model.PDD= Param()
+model.SOC=Param()
+model.n_in=Param()
+model.n_out=Param()
+model.Dt_in=Param()
+model.Dt_out=Param()
+model.P_in=Param()
+model.P_out=Param()
+model.Dt_p=Param()
+
+model.E_in=Var(within= NonNegativeReals)
+model.E_out=Var(within= NonNegativeReals)
+
+model.obj=Objective()

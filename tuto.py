ea9ae06044651d2c9fd3bd6b021524e51ddff90c
asbel apaza <dreik.elm.mec.game2@gmail.com>
2018-04-12 00:55:36 -0400

    dso

--- tuto.py
+++ tuto.py
@@ -18,10 +18,10 @@ def obj_rule(model):
 	return sum(model.E_out[1]*model.price[1])
 model.Obj= Objective(rule= obj_rule, sense= maximize)
 
-def con_rule(model):
-	#return (model.E_out[i] <= model.P_out*model.Dt_p for i in 10 )
+def con_rule(model,i):
+	#return (model.E_out[i] <= model.P_out*model.Dt_p)
 	return (model.E_out[1] <= model.P_out*model.Dt_p)
-model.Con1= Constraint(rule= con_rule)
+model.Con1= Constraint(model.hour, rule= con_rule)
 
 from pyomo.opt import SolverFactory
 opt = SolverFactory('glpk')
